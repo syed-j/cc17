d8ccdd1b2b4c29ca2857ff0949ada9b77c885262
Syed Jafri <ijtabajafripp@gmail.com>
2024-11-22 14:29:39 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import React from "react";
+import Gallery from "./Gallery";
+
+function App() {
+    return (
+        <div>
+            <h1>Tour Comparison App</h1>
+            <Gallery />
+        </div>
+    );
+}
+
+export default App;
